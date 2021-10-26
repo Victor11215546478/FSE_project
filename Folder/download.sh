08a07eada7af070ea4344b444e290b79da36abe2
Victor11215546478 <Crysisvictor@gmail.com>
2021-10-26 15:31:29 +0300

    add files

--- Folder/download.sh
+++
@@ -1,2 +0,0 @@
-wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip
-unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
---
+++ download.sh
@@ -0,0 +1,2 @@
+wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
+unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
